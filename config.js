e5f5761df0e27a25c76ab70f8ed9d59e7e0fa180
meherabmehu <jamimmhj432@gmail.com>
2026-08-19 10:07:32 +0600

    Save personal website configuration

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
   3) To enable the music button, put your own MP3 in music/ and name it: our-song.mp3
 */
 const SITE_CONFIG = {
-  SECRET_CODE: "cute-billi",
+  SECRET_CODE: "meherab",
   PHOTOS: [
     "images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg",
     "images/5.jpg", "images/6.jpg", "images/7.jpg", "images/8.jpg",
